940e77d6766ee968e97a7555a8c2fe9cc7099c71
Virgil Dupras <hsoft@hardcoded.net>
2019-02-05 20:56:29 -0500

    Move serial comm code to avr-common

--- drive/main.c
+++ drive/main.c
@@ -5,69 +5,13 @@
 
 #include "../common/pin.h"
 #include "../common/timer.h"
-#include "../common/intmath.h"
-
-#define DIGITS 4
+#include "../common/seg7serial.h"
 
 #define CLK PinB0
 #define SER PinB1
 
 static uint16_t val_to_send = 1234;
 volatile static bool should_send = false;
-
-void clock(bool high)
-{
-    pinlow(CLK);
-    pinset(SER, high);
-    /* After a little trial-and-error, this is the lowest delay I found with
-     * which I wouldn't get occasional erroring out on the other end. I find it
-     * a bit high (after all, the the same MCU on the other side, so the same
-     * speed). This indicates that the processing of input is more costly than
-     * I thought. Interesting...
-     */
-    _delay_us(300);
-    pinhigh(CLK);
-}
-
-void digit(uint8_t digit)
-{
-    if (digit >= 10) {
-        return;
-    }
-    clock(digit & 1);
-    clock(digit & (1 << 1));
-    clock(digit & (1 << 2));
-    clock(digit & (1 << 3));
-    clock(false); // no dot
-}
-
-void send(uint16_t val)
-{
-    uint8_t digits[DIGITS];
-    uint8_t verif = 0;
-
-    /* The computation below doesn't look like much, but it's actually intensive
-     * for the MCU. If it has to perform it while clocking has begun, we bust
-     * timeout, even if, in general, sending is much easier on the MCU than
-     * receiving (see _delay_us() comment in clock()). So, pre-compute before
-     * clocking.
-     */
-    for (int i=0; i < DIGITS; i++) {
-        digits[i] = (val / int_pow10(i)) % 10;
-        verif += digits[i];
-    }
-
-    // Empty CLK to begin
-    clock(false);
-    for (int i=0; i < DIGITS; i++) {
-        digit(digits[i]);
-    }
-    clock(verif & 1);
-    clock(verif & (1 << 1));
-    clock(verif & (1 << 2));
-    clock(verif & (1 << 3));
-    clock(verif & (1 << 4));
-}
 
 ISR(TIMER0_COMPA_vect)
 {
@@ -77,15 +21,14 @@ ISR(TIMER0_COMPA_vect)
 int main(void)
 {
     sei();
-    pinoutputmode(CLK);
-    pinoutputmode(SER);
+    seg7serial_setpins(CLK, SER);
     set_timer0_target(1000L * 1000L); // every second
     set_timer0_mode(TIMER_MODE_INTERRUPT);
 
     while (1) {
         if (should_send) {
             should_send = false;
-            send(val_to_send);
+            seg7serial_send(val_to_send);
             val_to_send++;
         }
     }
